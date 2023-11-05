1c35530273cc6ce1587673238208efbba87ce7e6
Aylex Nullzero <p4rlx-news@pm.me>
2023-11-06 02:50:35 +1030

    prepare to roll

--- setup.py
+++ setup.py
@@ -7,23 +7,16 @@ setup(
     author_email='p4rlx-news@pm.me',
     packages=find_packages(),
     license='LICENSE.txt',
-    description='An example Python package',
+    description='An MLOps and LLMOps Toolkit',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    install_requires=[
-        # Add your package dependencies here
-        # 'numpy',
-        # 'pandas',
-    ],
     classifiers=[
-        'Development Status :: 3 - Alpha',  # Change as appropriate
+        'Development Status :: 3 - Alpha',  
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # Change as appropriate
+        'License :: OSI Approved :: MIT License',  
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
